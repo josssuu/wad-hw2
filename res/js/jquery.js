7c77080ba885bb888e89ae76a1533fceb23fe2fb
Sandersorm <57254989+Sandersorm@users.noreply.github.com>
2020-10-25 21:45:51 +0200

    initButtonToggle function added

--- res/js/jquery.js
+++ res/js/jquery.js
@@ -1,3 +1,9 @@
+function initButtonToggle() {
+    $(".like-button").click(function () {
+        $(this).toggleClass("liked");
+    })
+}
+
 $(function () {
 
     loadUserInfo().then(function (response) {
@@ -11,6 +17,7 @@ $(function () {
         for (let post of response) {
             addPost(post)
         }
+        initButtonToggle();
     }).catch(function () {
         alert("Error loading posts info!")
     })
@@ -20,9 +27,7 @@ $(function () {
     })
 
 
-    $(".like-button").click(function () {
-        $(this).toggleClass("liked");
-    })
+
 })
 
 function loadUserInfo() {
